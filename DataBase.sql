e32aa7ae3c45d7c060e9160e4bd7b95c6fd8005c
Salvador Pamanes <s.pamanes@gmail.com>
2014-04-03 20:55:48 -0600

    modifique la base de datos ahora si bien, ya ligue las tablas, cree la tabla de rutina_ejercicio

--- DataBase.sql
+++ DataBase.sql
@@ -4,7 +4,7 @@ CREATE DATABASE appGym;
 CREATE USER 'appGym'@'localhost' IDENTIFIED BY 'ivdida';
 GRANT SELECT, DELETE, INSERT, UPDATE ON appGym.* TO 'sec_user'@'localhost';
 
-CREATE TABLE appGym.members (
+CREATE TABLE appGym.member (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -17,32 +17,50 @@ CREATE TABLE appGym.login_attempts (
     time VARCHAR(30) NOT NULL
 ) ENGINE=InnoDB;
 
-create table appGym.ejercicios(
-	idEjercicio int not null,
+create table appGym.ejercicio(
+	idEjercicio int not null primary key,
 	nombre varchar(30) not null,
 	descripcion varchar(30) not null,
 	musculo varchar(30) not null
 )engine = InnoDB;
 
-create table appGym.rutinas(
+create table appGym.rutina (
 	idAlumno int not null,
+	numRutina int not null,
+	dia0 date not null,
+	pesoInicial double not null,
+	pesoFinal double not null,
+	primary key (idAlumno, numRutina)
+)engine=InnoDB;
+
+
+create table appGym.rutina_ejercicio(
+	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	idAlumno int not null,
+	numRutina int not null,
 	idEjercicio int not null,
 	dia int not null,
+	
 	series int not null,
 	repeticiones int not null,
-	avance int not null
+	avance int not null,
+	primary key(idAlumno, numRutina, idEjercicio, dia), -- what??? 4 se necesitan para ser unico
+	foreign key(idEjercicio) references ejercicio(idEjercicio),
+	foreign key(idAlumno, numRutina) references rutina(idAlumno, numRutina)
 )engine = InnoDB;
 
 create table appGym.alumno(
-	idAlumno int not null,
+	idAlumno int not null primary key,
 	edad int not null,
 	peso double not null,
-	sexo varchar(2) not null
+	sexo varchar(2) not null,
+	foreign key idAlumno references member(id)
 )engine = InnoDB;
 
 create table appGym.Instructor(
-	idInstructor int not null,
-	tipo varchar(10)
+	idInstructor int not null primary key,
+	tipo varchar(10),
+	foreign key idInstructor references member(id)
 )engine = InnoDB;
 
 INSERT INTO appGym.members VALUES(1, 'test_user', 'test@example.com',
