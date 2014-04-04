0e50478a0477c310069e3b269dac7ce5c81293c6
Salvador Pamanes <s.pamanes@gmail.com>
2014-04-03 20:30:57 -0600

    commit 2

--- DataBase.sql
+++ DataBase.sql
@@ -19,3 +19,10 @@ CREATE TABLE `secure_login`.`login_attempts` (
 INSERT INTO `secure_login`.`members` VALUES(1, 'test_user', 'test@example.com',
 '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
+
+
+create table alumnos (
+	
+);
+
+create table 
--- getrutina.php
+++ getrutina.php
@@ -4,7 +4,15 @@
 
 $usuario = $_SESSION['username'];
 
-$conexion = new mysqli('appgym', '');
+$conexion = new mysqli('localhost', 'appGym', 'ivdida', 'appGym');
 
+if(mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+
+$result = $mysqli->query(
+	""
+) or die($mysqli->error.__LINE__);
 
 ?>
