d4153446a2f086cb2b9494ec801e0abe7582dd9e
Ivan <ivandiazmtz@gmail.com>
2014-04-08 19:17:58 -0500

    LO MAS NUEVO XCODE
    LO MAS NUEVO XCODE
    
    LO MAS NUEVO XCODE

---
+++ iLiftTec/iLiftTec/ILTLogInViewController.h
@@ -0,0 +1,16 @@
+//
+//  ILTLogInViewController.h
+//  iLiftTec
+//
+//  Created by Ivan Diaz on 4/3/14.
+//  Copyright (c) 2014 Ivan Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILTLogInViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *usuarioTF;
+@property (strong, nonatomic) IBOutlet UITextField *contrasenaTF;
+- (IBAction)loginButton:(id)sender;
+@end
---
+++ iLiftTec/iLiftTec/ILTLogInViewController.m
@@ -0,0 +1,126 @@
+//
+//  ILTLogInViewController.m
+//  iLiftTec
+//
+//  Created by Ivan Diaz on 4/3/14.
+//  Copyright (c) 2014 Ivan Diaz. All rights reserved.
+//
+
+#import "ILTLogInViewController.h"
+#import "SBJson.h"
+
+
+@interface ILTLogInViewController ()
+
+@end
+
+@implementation ILTLogInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void) alertStatus:(NSString *)msg :(NSString *)title
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:msg
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+    
+    [alertView show];
+}
+
+- (IBAction)loginButton:(id)sender {
+    
+    @try {
+        
+        if([[_usuarioTF text] isEqualToString:@""] || [[_contrasenaTF text] isEqualToString:@""] ) {
+            [self alertStatus:@"Please enter both Username and Password" :@"Login Failed!"];
+        } else {
+            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[_usuarioTF text],[_contrasenaTF text]];
+            NSLog(@"PostData: %@",post);
+            
+            NSURL *url=[NSURL URLWithString:@"http://localhost/login.php"];
+            
+            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+            
+            NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+            
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+            [request setURL:url];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:postData];
+            
+            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
+            
+            NSError *error = [[NSError alloc] init];
+            NSHTTPURLResponse *response = nil;
+            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            NSLog(@"Response code: %d", [response statusCode]);
+            if ([response statusCode] >=200 && [response statusCode] <300)
+            {
+                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                NSLog(@"Response ==> %@", responseData);
+                
+                SBJsonParser *jsonParser = [SBJsonParser new];
+                NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
+                NSLog(@"%@",jsonData);
+                NSInteger success = [(NSNumber *) [jsonData objectForKey:@"success"] integerValue];
+                NSLog(@"%d",success);
+                if(success == 1)
+                {
+                    NSLog(@"Login SUCCESS");
+                    [self performSegueWithIdentifier:@"correct" sender:self];
+                    //[self alertStatus:@"Logged in Successfully." :@"Login Success!"];
+                    
+                } else {
+                    
+                    NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
+                    [self alertStatus:error_msg :@"Login Failed!"];
+                }
+                
+            } else {
+                if (error) NSLog(@"Error: %@", error);
+                [self alertStatus:@"Connection Failed" :@"Login Failed!"];
+            }
+        }
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+        [self alertStatus:@"Login Failed." :@"Login Failed!"];
+    }
+}
+@end
